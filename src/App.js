e266e5a43ea435632cedf43024493c084578d5cd
omkarcode11 <omkarsonawaneomkar2@gmail.com>
2023-02-28 17:16:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { data } from "./assets/data";
+
+function Card({ data }) {
+  let grading = data.backgroundColor;
+  let rang = data.color;
+  console.log(grading, rang);
+  return (
+    <div
+      style={{ backgroundColor: `${grading}` }}
+      className='card-layout'>
+      <div className='icon-category'>
+        <div>{data.icon}</div>
+        <div style={{ color: `${rang}` }}>{data.category}</div>
+      </div>
+      <div>
+        <span className='in-mark'>{data.score}</span> <span className='outoff'> / 100</span>
+      </div>
+    </div>
+  );
+}
 
 function App() {
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='left-section'>
+        <div className='you-result' >Your Result</div >
+        <div className='score'>
+          <div className='result'>76</div>
+          <div className='total-mark'>of 100</div>
+        </div>
+
+        <div className='grade'>Great</div>
+        <div className='text-area'>
+          You score higher than 65% of the people who have taken these tests
+        </div>
+      </div>
+      <div className='right-section'>
+        <div className='category'>
+        <h3 className='summary'> Summary</h3>
+          {data.map((item, i) => (
+            <Card
+              data={item}
+              key={i}
+            />
+          ))}
+        </div>
+        <div className='button'>
+          <button className='continue-button'>Continue</button>
+        </div>
+      </div>
     </div>
   );
 }
